8e8e54ca0918c9c7b5f8b46070a50537e8b55271
Rifat Hasan <rhrifathasan1971@gmail.com>
2025-07-11 17:37:11 +0600

    WIP: Temporary changes to user module

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import { validateRequest } from "../../middlewares/validate.request";
 import { UserControllers } from "./user.controller";
+import { createUserZodSchema } from "./user.validator";
 
 const router = Router();
-router.post("/register", UserControllers.createUser);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+);
+router.get("/all-users", UserControllers.getAllUsers);
 
 export const UserRoutes = router;
